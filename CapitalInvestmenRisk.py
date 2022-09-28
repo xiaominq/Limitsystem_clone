17914c58e00be97f09af0d1b5a01b4b8963b8b52
b00812 <m.xiao@bvv.de>
2022-09-28 14:14:52 +0200

    fixed Typos, expand interface and class functionality refactoring and renaming of classes and methods adjusted riskmatrix

--- CapitalInvestmenRisk.py
+++ CapitalInvestmenRisk.py
@@ -1,9 +1,51 @@
-from Risk import Risk
+from abc import ABC
+from RiskInterface import RiskInterface
 
 
-class CapitalInvestmentRisk():
+class CapitalInvestmentRisk(RiskInterface, ABC):
 
     def __init__(self, name, category, loss):
         self.name = name
         self.category = category
         self.loss = loss
+
+    def getName(self):
+        return self.name
+
+    def getCategory(self):
+        return self.category
+
+    def getLossBrutto(self):
+        return self.loss
+
+    def getLossNetto(self):
+        return self.loss
+
+    def setLoss(self, value):
+        self.loss = value
+        return
+
+    def setLossBrutto(self, value):
+        self.setLoss(value)
+        return
+
+    def setLossNetto(self, value):
+        self.setLoss(value)
+        return
+
+    def setLossMid(self, value):
+        self.setLoss(value)
+        return
+
+    def setLossMin(self, value):
+        self.setLoss(value)
+        return
+
+    def lossMidCase(self):
+        return self.loss
+
+    def lossMinCase(self):
+        return self.loss
+
+    def lossRiskReduction(self):
+        return self.loss
--- RiskMatrix.py
+++ RiskMatrix.py
@@ -1,25 +1,24 @@
 
-
 riskPotential = {
-    0 : 1,
-    1 : 1,
+    0 : 0,
+    1 : 0.1,
     2 : 1,
-    3 : 4,
-    4 : 4,
-    5 : 8
+    3 : 10,
+    4 : 50,
+    5 : 200
 }
 
 riskProbability = {
-    0 : 1,
-    1 : 1,
-    2 : 1,
-    3 : 4,
-    4 : 4,
-    5 : 8
+    0 : 200,
+    1 : 20,
+    2 : 10,
+    3 : 5,
+    4 : 2,
+    5 : 1
 }
 
-def scaleProb2Value(s:int):
+def scaleProb2Value(s :int):
     return riskProbability[s]
 
-def scalePot2Value(s:int):
+def scalePot2Value(s :int):
     return riskPotential[s]
